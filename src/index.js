eaa93cc904887743200cd35be8e7d57e10e75d09
Sarah <sarahvue0924@gmail.com>
2024-07-01 20:23:39 -0700

    updated search functionality using api

--- src/index.js
+++ src/index.js
@@ -1,9 +1,25 @@
+function updateWeather(response) {
+  let tempElement = document.querySelector("#temperature");
+  let temp = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
+
+  cityElement.innerHTML = response.data.city;
+  tempElement.innerHTML = Math.round(temp);
+}
+
+function searchCity(city) {
+  let apiKey = "ao845a73f82d076c60a25fbb55dt1569";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=imperial`;
+  axios.get(apiUrl).then(updateWeather);
+}
+
 function handleSearch(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-  let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = searchInput.value;
+
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearch);
+searchCity("Los Angeles");
